8448cd7b65b12a4e871241f4bce8e8bfdc4e0cc2
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-19 13:50:49 +0100

    implementing the all shops routes

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const actionsManager = require('../db/actions');
 
 router.get('/', (req, res) => {
     //return All the shops using the db
+    actionsManager.allShops((shops)=>{
+        if(shops == null){
+            res.writeHead(500)
+        }else{
+            res.json(shops);
+        }
+    })
 });
 
 module.exports = router;
