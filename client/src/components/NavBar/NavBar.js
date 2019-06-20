de61cfc8381313efdce4db7ab398ba3053a9ad8c
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-20 15:12:39 +0100

    added a navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import logo from '../../assets/logo-shop.png';
+import { Navbar } from 'react-bootstrap';
+
 
 export class NavBar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="/">
+                        <img
+                            alt="Shop logo"
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        {'Shopsy'}
+                    </Navbar.Brand>
+                </Navbar>
+            </>
         )
     }
 }
