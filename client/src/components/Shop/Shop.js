1f1b55c7f2bd0ee8b53c95ab9894dae74cc7f14e
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-20 20:51:09 +0100

    implementing the shopList

--- client/src/components/Shop/Shop.js
+++ client/src/components/Shop/Shop.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Card, Button } from 'react-bootstrap';
 
 export class Shop extends Component {
     render() {
+        const {name, picture, email, city} = this.props;
         return (
-            <div>
-                
-            </div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <p>Contact us at : {email}</p>
+                        <p>Or come visit us at : {city}</p>
+                </Card.Text>
+                    <Button variant="primary">like</Button>
+                    <Button variant="danger">dislike</Button>
+                </Card.Body>
+            </Card>
         )
     }
 }
--- client/src/components/ShopList/ShopList.js
+++ client/src/components/ShopList/ShopList.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import Shop from '../Shop';
 
 export class ShopList extends Component {
+
+    calculateDistance(shopA){
+        let myCoordinates = this.myPosition();
+        return Math.sqrt(
+            Math.pow(shopA.location.coordinates[0] - myCoordinates[0],2)+ 
+            Math.pow(shopA.location.coordinates[1] - myCoordinates[1], 2)
+        ) 
+    }
+
+    myPosition(){
+        //this is going to utilize the GPS or something 
+        return [0,0]
+    }
+
     render() {
+        let {list} = this.props;
+        
+        //Probably should be smarter than this, This is going to be so slow 
+        list = list.sort((a,b)=>this.calculateDistance(a)<this.calculateDistance(b));
         return (
-            <div>
-                
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                v
-                This is the shop list
-                This is the shop list
-                This is the shop list
-                v
-            </div>
+            <>
+                <Container>
+                    <Row>
+                        {list.map(item=>
+                            <Col xs={3}>
+                                <Shop  city = {item.city}
+                                        name = {item.name}
+                                        picture = {item.picture}
+                                        email = {item.email}
+                                />
+                            </Col>
+                        )}
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
