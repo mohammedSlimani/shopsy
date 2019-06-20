86d46720e574b488bd1040704124f3e6eda2c0c6
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-20 14:13:50 +0100

    Implementing the API service manager.

---
+++ client/src/utils/Api.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const API_ROOT = 'https://smed-united.glitch.me/api';
+const TIMEOUT = 20000;
+const HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+};
+
+class ApiService {
+
+    constructor( baseURL = API_ROOT, timeout = TIMEOUT, headers = HEADERS ) {
+        const client = axios.create({
+            baseURL,
+            timeout,
+            headers
+        });
+
+        client.interceptors.response.use(this.handleSuccess, this.handleError);
+        this.client = client;
+
+    }
+
+    handleSuccess(response) {
+        return response;
+    }
+
+    handleError(error) {
+        return Promise.reject(error);
+    }
+
+    get(path) {
+        return this.client.get(path).then(response => response.data);
+    }
+
+    post(path, payload) {
+        return this.client.post(path, payload).then(response => response.data);
+    }
+
+    put(path, payload) {
+        return this.client.put(path, payload).then(response => response.data);
+    }
+}
+
+export default ApiService;
