02f94c2da514e9f4f2926ce002b5f24f2c4f5ea6
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-20 14:14:35 +0100

    Yey, I can sign up using my Client => All my routes are fine.

--- client/src/components/SignInUp/SignIn/SignIn.js
+++ client/src/components/SignInUp/SignIn/SignIn.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import * as yup from 'yup'
 import { Formik } from 'formik';
 import { Container, InputGroup, Button, Form } from 'react-bootstrap';
+import ApiService from '../../../utils/Api';
 
 const schema = yup.object({
-    userName: yup
+    email: yup
         .string()
         .email()
         .required(),
@@ -16,8 +17,10 @@ const schema = yup.object({
 
 
 export class SignIn extends Component {
-    login() {
-        console.log('loggin you in');
+    async login(payload) {
+        const myApi = new ApiService();
+        const data = await myApi.post('/users/sign-up',payload);
+        console.log(data);
     }
 
     render() {
@@ -34,8 +37,8 @@ export class SignIn extends Component {
                 }) => (
                         <Container>
                             <Form noValidate onSubmit={handleSubmit}>
-                                <Form.Group controlId="validationFormikUserName">
-                                    <Form.Label>userName</Form.Label>
+                                <Form.Group controlId="validationFormikEmail">
+                                    <Form.Label>email</Form.Label>
                                     <InputGroup>
                                         <InputGroup.Prepend>
                                             <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
@@ -44,13 +47,13 @@ export class SignIn extends Component {
                                             type="text"
                                             placeholder="user@company.com"
                                             aria-describedby="inputGroupPrepend"
-                                            name="userName"
-                                            value={values.userName}
+                                            name="email"
+                                            value={values.email}
                                             onChange={handleChange}
-                                            isInvalid={!!errors.userName}
+                                            isInvalid={!!errors.email}
                                         />
                                         <Form.Control.Feedback type="invalid">
-                                            {errors.userName}
+                                            {errors.email}
                                         </Form.Control.Feedback>
                                     </InputGroup>
                                 </Form.Group>
