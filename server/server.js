13847780c7f32fb5ee795fb0644ca455b71a6c79
Mohammed slimani <slimani.med.21@gmail.com>
2019-07-12 11:22:12 +0100

    Session initialized

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const usersRoutes = require('./routes/users');
 const shopsRoutes = require('./routes/shops');
 
@@ -14,6 +15,8 @@ app.use(session({
   key: 'express.sid',
   store: sessionStore,
 }));
+
+
 //Configuration of the app
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
