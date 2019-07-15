1e47a294ae687261d213429657a1ad4d1eecff97
Mohammed slimani <slimani.med.21@gmail.com>
2019-07-15 18:59:23 +0100

    implementing the Facebook Oauth

--- server/Auth/config.js
+++ server/Auth/config.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require("./Model").User;
 
 passport.serializeUser((user, done) => {
@@ -10,17 +11,12 @@ passport.deserializeUser((id, done) => {
     User.findOne(
         { id: id },
         (err, doc) => {
-            console.log("err deserialize", err);
             done(null, doc);
         }
     );
 });
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "https://smed-united.glitch.me/api/users/auth/github/callback"
-}, function (accessToken, refreshToken, profile, cb) {
+const registerUser = (accessToken, refreshToken, profile, done) => {
     User.findOneAndUpdate(
         { id: profile.id },
         {
@@ -29,10 +25,25 @@ passport.use(new GitHubStrategy({
         },
         { upsert: true, new: true },//Insert object if not found, Return new object after modify
         (err, doc) => {
-            //console.log('profile',profile);
+            console.log('profile', profile);
             console.log("err", err);
             console.log('doc to add,', doc);
-            return cb(null, doc);
+            return done(null, doc);
         })
 }
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "https://smed-united.glitch.me/api/users/auth/github/callback"
+}, registerUser
 ));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "https://smed-united.glitch.me/api/users/auth/facebook/callback"
+}, registerUser
+))
+
+
