05c147b611e7aa48665be71f2be5db7a687837b2
Mohammed slimani <slimani.med.21@gmail.com>
2019-07-15 18:18:18 +0100

    Tweaking the model to be more forgiving

--- server/Auth/Model.js
+++ server/Auth/Model.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userProto = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    prefered: [{ type: Schema.Types.ObjectId, ref: 'shops' }]
+    email: { type: String, unique: true },
+    password: String,
+    prefered: [{ type: Schema.Types.ObjectId, ref: 'shops' }],
+    name: String,
+    id: Number // This is a path for the github id until i find a proper way out 
 })
 
 let User = mongoose.model('users', userProto);
