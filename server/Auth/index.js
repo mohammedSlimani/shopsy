72f2575a8108c5653b4fba1bbcd80180e49570d9
Mohammed slimani <slimani.med.21@gmail.com>
2019-07-15 18:17:55 +0100

    implementing the Routes of the github strategy

--- server/Auth/index.js
+++ server/Auth/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const signIn = require('./db').signIn;
 const signUp = require('./db').signUp;
+const passport = require('passport');
+const passportConfig = require('./config');
 
 router.post('/sign-in', (req, res) => {
     //Return a user or an error.
@@ -32,6 +34,37 @@ router.post('/sign-up', (req, res) => {
                 .json(user);
         }
     });
+});
+
+router.get('/auth/github', passport.authenticate('github'));
+
+router.get('/auth/github/callback',
+    passport.authenticate('github', {
+        failureRedirect: '/api/users/auth/github/failed',
+        successRedirect: '/api/users/auth/github/success'
+    }
+    ))
+
+router.get('/auth/github/failed', (req, res) => {
+    res.status(401).json({
+        success: false,
+        message: "user failed to authenticate."
+    });
+});
+
+router.get('/auth/github/success', (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            success: true,
+            message: "user has successfully authenticated",
+            user: req.user,
+            cookies: req.cookies
+        });
+    } else {
+        req.status(500).json({
+            err: "Success but no user ?"
+        })
+    }
 });
 
 module.exports = router;
